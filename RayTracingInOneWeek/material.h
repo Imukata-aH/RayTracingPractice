055daaebc55be1db999f4c02525d3db8f8241a5b
Imukata-aH <leahyleahyleahy@gmail.com>
2016-09-13 00:09:24 +0900

    reflect()をmaterialのメソッドにする。

--- RayTracingInOneWeek/material.h
+++ RayTracingInOneWeek/material.h
@@ -1,11 +1,36 @@
 #pragma once
 
+#include "vec3.h"
+
 class ray;
 struct HitRecord;
-class vec3;
 
 class material
 {
 public:
 	virtual bool scatter(const ray& r_in, const HitRecord& rec, vec3& attenuation, ray& scattered) const = 0;
+protected:
+	vec3 reflect(const vec3& v, const vec3& n) const
+	{
+		return v - 2 * dot(v, n) * n;
+	}
+
+	bool refract(const vec3& v, const vec3& n, float ni, float nt, vec3& refracted) const	// ni: N“üŒ³‹üÜ—¦ nt: N“üæ‹üÜ—¦
+	{
+		float niOverNt{ ni / nt };
+		vec3 uv{ unit_vector(v) };
+		float dt{ dot(uv, n) };
+		float discriminant{ 1.0f - niOverNt*niOverNt*(1 - dt*dt) };
+		if (discriminant > 0) 
+		{
+			// ‹üÜ‚µ‚½
+			refracted = niOverNt*(uv - n*dt) - n * sqrt(discriminant);
+			return true;
+		}
+		else
+		{
+			// ‘S”½Ë
+			return false;
+		}
+	}
 };
--- RayTracingInOneWeek/metal.cpp
+++ RayTracingInOneWeek/metal.cpp
@@ -4,14 +4,9 @@
 #include "vec3.h"
 #include "random_util.h"
 
-vec3 reflect(const vec3& v, const vec3& n)
-{
-	return v - 2 * dot(v, n) * n;
-}
-
 bool Metal::scatter(const ray& r_in, const HitRecord& rec, vec3& attenuation, ray& scattered) const
 {
-	vec3 reflected = reflect(unit_vector(r_in.diretion()), rec.normal);
+	vec3 reflected = material::reflect(unit_vector(r_in.diretion()), rec.normal);
 	scattered = ray(rec.hitPoint, reflected + fuzz * RandomUtil::getrRandomPointInUnitSphere());
 	attenuation = albedo;
 	return (dot(scattered.diretion(), rec.normal) > 0);
