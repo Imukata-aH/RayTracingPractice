deca489ae3afaefa26030e8a450c2405d71bbf63
Imukata-aH <leahyleahyleahy@gmail.com>
2016-09-19 13:52:29 +0900

    Add a variable of time when a ray exists to ray class

--- RayTracingInOneWeek/ray.h
+++ RayTracingInOneWeek/ray.h
@@ -5,11 +5,13 @@ class ray
 {
 public:
 	ray(){}
-	ray(const vec3& origin, const vec3& direction) { A = origin, B = direction; }
+	ray(const vec3& origin, const vec3& direction, float time = 0.0f) : A{ origin }, B{ direction }, mTime{ time } {}
 	vec3 origin()const { return A; }
 	vec3 diretion()const { return B; }
+	float time() const { return mTime; }
 	vec3 point_at_parameter(float t)const { return A + t*B; }
-
+private:
 	vec3 A;
 	vec3 B;
+	float mTime;	// time when a ray exists
 };
