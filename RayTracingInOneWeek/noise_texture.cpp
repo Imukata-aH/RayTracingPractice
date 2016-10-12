4b90158dac8c356358da229294ff9d7b97a5ce06
Imukata-aH <leahyleahyleahy@gmail.com>
2016-10-12 09:25:45 +0900

    Added scaling hit point of NoiseTexture to make noise change more frequently.

--- RayTracingInOneWeek/noise_texture.cpp
+++ RayTracingInOneWeek/noise_texture.cpp
@@ -5,7 +5,11 @@ NoiseTexture::NoiseTexture()
 {
 }
 
+NoiseTexture::NoiseTexture(float scale) : scale{ scale }
+{
+}
+
 vec3 NoiseTexture::value(float u, float v, const vec3& p) const
 {
-	return vec3(1, 1, 1) * noise.noise(p);
+	return vec3(1, 1, 1) * noise.noise(scale * p);
 }
--- RayTracingInOneWeek/noise_texture.h
+++ RayTracingInOneWeek/noise_texture.h
@@ -7,8 +7,10 @@ class NoiseTexture : public Texture
 {
 public:
 	NoiseTexture();
+	NoiseTexture(float scale);
 	virtual vec3 value(float u, float v, const vec3& p) const;
 
 private:
 	Perlin noise;
+	float scale;
 };
