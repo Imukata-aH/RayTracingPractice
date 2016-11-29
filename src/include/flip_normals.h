5e90759e916a1a8efb18f1ce65f3b4a7511843e1
Imukata-aH <leahyleahyleahy@gmail.com>
2016-11-29 14:55:57 +0900

    Add FlipNormals class.
    This wrap a Hitable object and flip its normal when a ray hits.

---
+++ src/include/flip_normals.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "hitable.h"
+
+// Hitable object that has inverse normal which it wrap
+class FlipNormals : public Hitable
+{
+public:
+	FlipNormals(Hitable *object);
+	virtual bool hit(const ray& r, float tMin, float tMax, HitRecord& rec) const;
+	virtual bool boundingBox(float t0, float t1, AABB& box) const;
+
+private:
+	Hitable* obj;
+};
---
+++ src/src/flip_normals.cpp
@@ -0,0 +1,25 @@
+﻿#include "flip_normals.h"
+
+FlipNormals::FlipNormals(Hitable * object):
+	obj{object}
+{
+}
+
+bool FlipNormals::hit(const ray & r, float tMin, float tMax, HitRecord & rec) const
+{
+	if (obj->hit(r, tMin, tMax, rec))
+	{
+		// flip normal
+		rec.normal = -rec.normal;
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool FlipNormals::boundingBox(float t0, float t1, AABB & box) const
+{
+	return obj->boundingBox(t0, t1, box);
+}
